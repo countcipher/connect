0d52bbb0229397b39e0a55273bdf44bf3879cc70
Count Cipher <46378241+countcipher@users.noreply.github.com>
2019-01-13 15:48:23 -0500

    Updated db credentials to template version

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "mes_admin1123";
-$password = "cOQgp2dIBHfBMm3x";
-$dbase = "cl_connect";
+$host = "";//host
+$user = "";//username
+$password = "";//user password
+$dbase = "";//database name
 
 $connection = mysqli_connect($host, $user, $password, $dbase);
 
